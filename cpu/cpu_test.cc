185557ecd55ea113fcc22f612c75707d7299062d
thanhld94 <thanhld94@gmail.com>
2016-10-14 14:37:17 -0500

    testing ADC addressing mode

--- cpu/cpu_test.cc
+++ cpu/cpu_test.cc
@@ -2,6 +2,20 @@
 
 #include "gtest/gtest.h"
 
+#define IMMEDIATE   0
+#define ZEROPAGE    1
+#define ZEROPAGEX   2
+#define ZEROPAGEY   3
+#define ABSOLUTE    4
+#define ABSOLUTEX   5
+#define ABSOLUTEY   6
+#define INDIRECTX   7
+#define INDIRECTY   8
+#define ACCUMULATOR 9
+#define RELATIVE    10
+#define IMPLIED     11
+#define INDIRECT    12
+
 TEST (InitializeTest, FirstState) {
   CPU cpu;
   EXPECT_EQ(cpu.get_pc(), 0) << "initial program counter should be zero";
@@ -10,4 +24,16 @@ TEST (InitializeTest, FirstState) {
   EXPECT_EQ(cpu.get_ry(), 0) << "initial value of Register Y should be zero";
   EXPECT_EQ(cpu.get_acc(), 0) << "initial value of accumulator should be zero";
   EXPECT_EQ(cpu.get_st(), 0) << "inital state of status register should all be cleared";
+}
+
+TEST (AddressingModeTest, ADC_AddWithCarry) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0x69), IMMEDIATE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x65), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x75), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x6D), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x7D), ABSOLUTEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x79), ABSOLUTEY);
+  EXPECT_EQ(cpu.get_addressing_mode(0x61), INDIRECTX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x71), INDIRECTY);
 }
