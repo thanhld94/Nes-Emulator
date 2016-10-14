1c7bbaa2d59c463219c545013c54a6a8070db9e8
thanhld94 <thanhld94@gmail.com>
2016-10-14 15:57:02 -0500

    Fixing LRS addressing, testing DECs JMPs INCs and LDs

--- cpu/cpu_test.cc
+++ cpu/cpu_test.cc
@@ -135,6 +135,13 @@ TEST (AddressingModeTest, CMP_Compare) {
   EXPECT_EQ(cpu.get_addressing_mode(0xD9), ABSOLUTEY);
   EXPECT_EQ(cpu.get_addressing_mode(0xC1), INDIRECTX);
   EXPECT_EQ(cpu.get_addressing_mode(0xD1), INDIRECTY);
+  EXPECT_EQ(cpu.get_addressing_mode(0xC5), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xD5), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xCD), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xDD), ABSOLUTEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xD9), ABSOLUTEY);
+  EXPECT_EQ(cpu.get_addressing_mode(0xC1), INDIRECTX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xD1), INDIRECTY);
 }
 
 TEST (AddressingModeTest, CPX_CompareXRegister) {
@@ -149,5 +156,102 @@ TEST (AddressingModeTest, CPY_CompareYRegister) {
   EXPECT_EQ(cpu.get_addressing_mode(0xC0), IMMEDIATE);
   EXPECT_EQ(cpu.get_addressing_mode(0xC4), ZEROPAGE);
   EXPECT_EQ(cpu.get_addressing_mode(0xCC), ABSOLUTE);
+}
 
+TEST (AddressingModeTest, DEC_DecrementMemory) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xC6), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xD6), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xCE), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xDE), ABSOLUTEX);
+}
+
+TEST (AddressingModeTest, DEX_DecrementXRegister) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xCA), IMPLIED); 
+}
+
+TEST (AddressingModeTest, DEY_DecrementYRegister) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0x88), IMPLIED); 
+}
+
+TEST (AddressingModeTest, XOR_ExclusiveOR) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0x49), IMMEDIATE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x45), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x55), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x4D), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x5D), ABSOLUTEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x59), ABSOLUTEY);
+  EXPECT_EQ(cpu.get_addressing_mode(0x41), INDIRECTX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x51), INDIRECTY);
+}
+
+TEST (AddressingModeTest, INC_IncrementMemory) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xE6), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xF6), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xEE), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xFE), ABSOLUTEX);
+} 
+
+TEST (AddressingModeTest, INX_IncrementXRegister) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xE8), IMPLIED);
+}
+
+TEST (AddressingModeTest, INY_IncrementYRegister) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xC8), IMPLIED);
+}
+
+TEST (AddressingModeTest, JMP_Jump) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0x4C), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x6C), INDIRECT);
+}
+
+TEST (AddressingModeTest, JSR_JumpToSubroutine) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0x20), ABSOLUTE);
+}
+
+TEST (AddressingModeTest, LDA_LoadAccumulator) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xA9), IMMEDIATE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xA5), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xB5), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xAD), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xBD), ABSOLUTEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xB9), ABSOLUTEY);
+  EXPECT_EQ(cpu.get_addressing_mode(0xA1), INDIRECTX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xB1), INDIRECTY);
+}
+
+TEST (AddressingModeTest, LDX_LoadXRegister) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xA2), IMMEDIATE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xA6), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xB6), ZEROPAGEY);
+  EXPECT_EQ(cpu.get_addressing_mode(0xAE), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xBE), ABSOLUTEY);
+}
+
+TEST (AddressingModeTest, LDY_LoadYRegister) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0xA0), IMMEDIATE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xA4), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xB4), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0xAC), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0xBC), ABSOLUTEX);
+}
+
+TEST (AddressingModeTest, LSR_LogicalShiftRight) {
+  CPU cpu;
+  EXPECT_EQ(cpu.get_addressing_mode(0x4A), ACCUMULATOR);
+  EXPECT_EQ(cpu.get_addressing_mode(0x46), ZEROPAGE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x56), ZEROPAGEX);
+  EXPECT_EQ(cpu.get_addressing_mode(0x4E), ABSOLUTE);
+  EXPECT_EQ(cpu.get_addressing_mode(0x5E), ABSOLUTEX);
 }
