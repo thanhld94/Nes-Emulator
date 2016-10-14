789eabe9bd3394a4f8924ce462d7d0448814c888
thanhld94 <thanhld94@gmail.com>
2016-10-14 14:36:52 -0500

    get addressing mode

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -4,6 +4,11 @@
 class CPU {
   public:
     CPU();
+
+    /* Addressing Mode */
+    int get_addressing_mode(uint8_t opcode);
+
+    /* Getters & Setters*/
     uint16_t get_pc();
     uint8_t get_sp();
     uint8_t get_rx();
@@ -18,4 +23,6 @@ class CPU {
     uint8_t r_y;
     uint8_t r_acc;
     uint8_t r_st;
+
+    static const int mode_table[256]; 
 };
