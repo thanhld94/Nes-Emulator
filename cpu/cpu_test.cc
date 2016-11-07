1015285dbdf6b637fb232063dda2b2488994de98
thanhld94 <thanhld94@gmail.com>
2016-11-07 17:44:23 -0600

    Testing clock cycles - BCC without page different

--- cpu/cpu_test.cc
+++ cpu/cpu_test.cc
@@ -1265,6 +1265,15 @@ TEST (ClockCycleTest, ASL_ArithmeticShiftLeft) {
   }
 }
 
+TEST (ClockCycleTest, BCC_BranchIfCarryClear) {
+  CPU cpu;
+  int pc = cpu.get_pc();
+  cpu.set_memory(pc, 0x90); // bcc
+  int expected = cpu.get_cycles() + 2;
+  cpu.step();
+  EXPECT_EQ(cpu.get_cycles(), expected);
+}
+
 //TODO hande instruction cycles with page different
 
 } // namespace nesemu
