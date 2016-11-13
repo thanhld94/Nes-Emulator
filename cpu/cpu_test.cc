7679a767ce9b9426b1d55ab6322f0331b4a1eee3
thanhld94 <thanhld94@gmail.com>
2016-11-13 13:11:01 -0600

    Testing clock cycles - BNE without page different - Handle successful and unsuccessful branch

--- cpu/cpu_test.cc
+++ cpu/cpu_test.cc
@@ -1368,6 +1368,27 @@ TEST (ClockCycleTest, BMI_BranchIfMinus) {
   }
 }
 
+TEST (ClockCycleTest, BNE_BranchIfNotEqual) {
+  {// unsuccessful branch
+    CPU cpu;
+    int pc = cpu.get_pc();
+    cpu.set_zero();
+    cpu.set_memory(pc, 0xD0); // bne
+    int expected = cpu.get_cycles() + 2;
+    cpu.step();
+    EXPECT_EQ(cpu.get_cycles(), expected);
+  }
+  {// successful branch
+    CPU cpu;
+    int pc = cpu.get_pc();
+    cpu.clear_zero();
+    cpu.set_memory(pc, 0xD0); // bne
+    int expected = cpu.get_cycles() + 3;
+    cpu.step();
+    EXPECT_EQ(cpu.get_cycles(), expected);
+  }
+}
+
 //TODO hande instruction cycles with page different
 
 } // namespace nesemu
